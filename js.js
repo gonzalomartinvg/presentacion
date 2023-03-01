973fd1e141b8261d3f2f37a2f7d104466dc2b955
Gonzalo <gonzalomartinvg@gmail.com>
2023-03-01 16:56:32 -0300

    primer commit

--- js.js
+++ js.js
@@ -44,6 +44,7 @@ setInterval(hitch, 5600)
 //Botón conoceme
 
 let boton = document.querySelector(".boton");
+let titulo = document.querySelector(".titulo");
 let contenedorFoto = document.querySelector(".contenedor-foto");
 let contenedorParrafo = document.querySelector(".contenedor-parrafo");
 
@@ -51,6 +52,8 @@ boton.addEventListener("click", cambiarInformacion);
 
 function cambiarInformacion(){
 
+    titulo.textContent = "Gonzalo Volpe Gómez"
+    
     contenedorFoto.innerHTML = '';
 
     contenedorParrafo.innerHTML = '<p class="parrafo movimiento">La foto anterior es de un banco de imágenes, no soy yo. En la web y en las redes sociales muchas personas no son quienes dicen ser. Adultos engañas a niños haciéndose pasar por otros, filtros nos desfiguran a un punto irreconocible pero los seguimos usando para encajar en el estandar de belleza, CV y perfiles de linkedin llenos de hazañas, aptitudes asombrosas y ningún fracaso.</p> <p class="parrafo movimiento">Soy Gonzalo Volpe Gómez, podés conocer lo que quiero mostrar de mi en <a class="hipervinculo" href="https://www.linkedin.com/in/gonzalo-martin-volpe-gomez">Linkedin</a> y mis proyectos en <a class="hipervinculo" href="https://gonzalomartinvg.com/">www.gonzalomartinvg.com</a></p> <p class="parrafo movimiento">O podés invitarme a una entrevista y conocerme en persona <a class="hipervinculo" href="https://wa.me/5491148706906">+5491148706906</a></p>';
